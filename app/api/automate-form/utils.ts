c08294f7a7f60b7c5168e00b3f954ef1ed0e1825
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2024-09-01 13:34:34 +0700

    config executable path

--- app/api/automate-form/utils.ts
+++ app/api/automate-form/utils.ts
@@ -3,15 +3,8 @@ import { Browser } from "puppeteer";
 export const initBrowser = async () => {
   let browser: Browser | undefined | null;
   if (process.env.NODE_ENV !== "development") {
-    const chromium = require("@sparticuz/chromium");
-    // Optional: If you'd like to disable webgl, true is the default.
-    chromium.setGraphicsMode = false;
     const puppeteer = require("puppeteer-core");
     browser = await puppeteer.launch({
-      args: chromium.args,
-      executablePath: await chromium.executablePath(
-        "https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar"
-      ),
       headless: true,
     });
   } else {
